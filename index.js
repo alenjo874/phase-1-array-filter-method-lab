5419fe4e6709420c84cba875b71d04f1dcecc541
Alen Jo <alenjo874@gmail.com>
2022-01-12 10:55:45 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  const result = drivers.filter(
+    (driver) => driver.toUpperCase() === string.toUpperCase()
+  );
+  return result;
+}
+
+function fuzzyMatch(drivers, string) {
+  let sLength = string.length;
+  const result = drivers.filter(
+    (driver) => driver.slice(0, sLength) === string
+  );
+  return result;
+}
+
+function matchName(drivers, string) {
+  const result = drivers.filter((driver) => driver.name === string);
+  return result;
+}
